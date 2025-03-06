71df9fbd180d3480b50230b4ed5c81c78591f91e
Dorian W <idorianw@gmail.com>
2025-03-06 21:13:47 +0100

    Updating files

--- server-monitoring/deamon.py
+++ server-monitoring/deamon.py
@@ -32,7 +32,9 @@ def get_cpu_graph():
             "--width=800", "--height=400",
             "DEF:cpu_usage=system.rrd:cpu_usage:AVERAGE",
             "DEF:io_delay=system.rrd:io_delay:AVERAGE",
-            "LINE2:cpu_usage#94AE0A:CPU Usage (%)",
+  	    "AREA:cpu_usage#94AE0A40:CPU Usage (%)",
+     	    "AREA:io_delay#115FA640",
+            "LINE2:cpu_usage#94AE0A",
             "LINE2:io_delay#115FA6:IODelay (%)"
         ])
         return send_file(graph_path, mimetype='image/png')
@@ -50,7 +52,11 @@ def get_memory_graph():
             "--width=800", "--height=400",
             "DEF:total_mem=system.rrd:total_mem:AVERAGE",
             "DEF:used_mem=system.rrd:used_mem:AVERAGE",
-            "LINE2:total_mem#94AE0A:Total Memory (MB)",
+            "CDEF:total_mem_gb=total_mem,1,*",
+    	    "CDEF:used_mem_gb=used_mem,1,*",
+	    "AREA:total_mem#94AE0A40",
+	    "AREA:used_mem#115FA640",
+ 	    "LINE2:total_mem#94AE0A:Total Memory (MB)",
             "LINE2:used_mem#115FA6:Used Memory (MB)"
         ])
         return send_file(graph_path, mimetype='image/png')
@@ -67,9 +73,13 @@ def get_disk_graph():
             "--title=Disk Usage",
             "--width=800", "--height=400",
             "DEF:total_disk=system.rrd:total_disk:AVERAGE",
-            "DEF:used_disk=system.rrd:used_disk:AVERAGE",
-            "LINE2:total_disk#94AE0A:Total Disk Space (GB)",
-            "LINE2:used_disk#115FA6:Used Disk Space (GB)"
+    	    "DEF:used_disk=system.rrd:used_disk:AVERAGE",
+    	    "CDEF:total_disk_gb=total_disk,1,*",
+            "CDEF:used_disk_gb=used_disk,1,*",
+	    "AREA:total_disk#94AE0A40",
+            "AREA:used_disk#115FA640",
+    	    "LINE2:total_disk_gb#94AE0A:Total Disk Space (GB)",
+            "LINE2:used_disk_gb#115FA6:Used Disk Space (GB)"
         ])
         return send_file(graph_path, mimetype='image/png')
     except Exception as e:
--- server-monitoring/update_rrd.sh
+++ server-monitoring/update_rrd.sh
@@ -1,23 +1,30 @@
 #!/bin/bash
 
+
 # Get System Values
 cpu_usage=$(ps aux --sort=-%cpu | head -n 2 | tail -n 1 | awk '{print $3}')
 io_delay=$(iostat -x | grep '^sda' | awk '{print $4}')
 
-total_mem=$(free -m | grep Mem | awk '{print $2}')
-used_mem=$(free -m | grep Mem | awk '{print $3}')
+total_mem=$(free -b | grep Mem | awk '{print $2}')
+used_mem=$(free -b | grep Mem | awk '{print $3}')
 
-total_disk=$(df / | tail -1 | awk '{print $2}')
-used_disk=$(df / | tail -1 | awk '{print $3}')
+total_disk=$(df -B1 / | tail -1 | awk '{print $2}')
+used_disk=$(df -B1 / | tail -1 | awk '{print $3}')
 
 
-# Data Processing
-total_mem_gb=$(echo "scale=2; $total_mem/1024" | bc)
-used_mem_gb=$(echo "scale=2; $used_mem/1024" | bc)
+# Processing Data
+total_mem_gb=$(echo "scale=2; $total_mem / 1073741824" | bc)
+used_mem_gb=$(echo "scale=2; $used_mem / 1073741824" | bc)
 
-total_disk_gb=$(echo "scale=2; $total_disk/1048576" | bc)
-used_disk_gb=$(echo "scale=2; $used_disk/1048576" | bc)
+total_disk_gb=$(echo "scale=2; $total_disk / 1073741824" | bc)
+used_disk_gb=$(echo "scale=2; $used_disk / 1073741824" | bc)
+
+
+# Debug
+echo "CPU: $cpu_usage, IO Delay: $io_delay"
+echo "Total Mem: $total_mem bytes, Used Mem: $used_mem bytes"
+echo "Total Disk: $total_disk bytes, Used Disk: $used_disk bytes"
 
 
 # Updating RRD Database
-rrdtool update system.rrd N:$cpu_usage:$io_delay:$total_mem:$used_mem:$total_disk:$used_disk
+rrdtool update /opt/server-monitoring/system.rrd N:$cpu_usage:$io_delay:$total_mem:$used_mem:$total_disk:$used_disk
